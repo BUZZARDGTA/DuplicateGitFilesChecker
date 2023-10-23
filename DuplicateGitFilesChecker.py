1328429e47193876c9be6c3af4b1274f116580b5
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-10-23 11:49:58 +0200

    Update DuplicateGitFilesChecker.py

--- DuplicateGitFilesChecker.py
+++ DuplicateGitFilesChecker.py
@@ -6,17 +6,14 @@ from pathlib import Path
 GIT_REPO = Path("D:/Git/Illegal_Services/GitHub")
 
 
-def run_git_command(command):
-    try:
-        output = subprocess.check_output(command)
-        output = output.decode().splitlines(keepends=False)
-        return output
-    except subprocess.CalledProcessError as e:
-        raise("Error running the command:", e)
+def execute(command):
+    output = subprocess.check_output(command)
+    return output
 
 
 os.chdir(GIT_REPO)
-lines = run_git_command("git ls-files")
+output = execute("git ls-files")
+lines = output.decode().splitlines(keepends=False)
 lines_lowercase = [line.lower() for line in lines]
 
 seen_lines = set()
