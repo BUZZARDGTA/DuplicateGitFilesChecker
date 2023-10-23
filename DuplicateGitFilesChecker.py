5cc2e214de7d9c834dbba15d1f4ef7e7b6810ef0
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-10-23 11:21:23 +0200

    Add files via upload

---
+++ DuplicateGitFilesChecker.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+from pathlib import Path
+
+
+git_repo_directory = Path("D:/Git/Illegal_Services/GitHub")
+os.chdir(git_repo_directory)
+
+command = "git ls-files"
+try:
+    output = subprocess.check_output(command, shell=True, text=True)
+except subprocess.CalledProcessError as e:
+    print("Error running the command:", e)
+    exit(0)
+
+# Split the output into lines and convert them to lowercase
+lines = output.split("\n")
+lines_lowercase = [line.lower() for line in lines]
+
+# Create a set to keep track of seen lines (case-insensitive)
+seen_lines = set()
+duplicates = set()
+
+# Check for duplicates (case-insensitive) and print them
+for line, line_lower in zip(lines, lines_lowercase):
+    if line_lower in seen_lines:
+        duplicates.add(line)
+    seen_lines.add(line_lower)
+
+if duplicates:
+    print("Duplicates (case-insensitive):")
+    for duplicate in sorted(duplicates, key=str.lower):
+        print(duplicate)
