831af65b0df3543412e019ddda01e36f68446dfc
IB_U_Z_Z_A_R_Dl <BUZZARDGTA@protonmail.com>
2024-12-19 20:59:41 +0100

    Update DuplicateGitFilesChecker.py

--- DuplicateGitFilesChecker.py
+++ DuplicateGitFilesChecker.py
@@ -1,11 +1,10 @@
-import os
 import sys
 import subprocess
 from pathlib import Path
 
 
-def execute(command):
-    output = subprocess.check_output(command)
+def execute(command, cwd=None):
+    output = subprocess.check_output(command, cwd=cwd)
     return output
 
 
@@ -13,14 +12,14 @@ if len(sys.argv) != 2:
     print('Usage: python "DuplicateGitFilesChecker.py" "<git_repo_path>"')
     sys.exit(1)
 
-git_repo_path = Path(sys.argv[1])
-os.chdir(git_repo_path)
-output = execute("git ls-files")
+git_repo_path = Path(sys.argv[1]).resolve()
+output = execute("git ls-files", cwd=git_repo_path)
+
 lines = output.decode().splitlines(keepends=False)
 lines_lowercase = [line.lower() for line in lines]
 
-seen_lines = set()
-duplicates = set()
+seen_lines: set[str] = set()
+duplicates: set[str] = set()
 
 # Check for duplicates (case-insensitive) and print them
 for line, line_lower in zip(lines, lines_lowercase):
